a5888b46780b17b791e0da772c1983a69d74a6d2
Ida <ida.tasanen@gmail.com>
2018-10-21 22:18:56 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,5 +9,5 @@ var quotes = [
 $(function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.lenght));
   document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-
+$()
 }
