a7a93c13250b7787959688881e1e8c31d73452ca
Ida <ida.tasanen@gmail.com>
2018-10-21 22:09:33 +0200

    lunch2

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var quotes = [
+  'Portal'
+  'Matateljen'
+  'WKB'
+  'Sallad'
+
+]
+
+function  newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.lenght));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+
+}
